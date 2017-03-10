87e06be13d3c0d3a2a4adf4e2b69fb92626937d0
Andrew Carpenter <andrew@audioinhd.com>
2017-03-10 17:30:41 -0500

    Initial commit, completely untested

---
+++ README.md
@@ -0,0 +1,48 @@
+# pg-ears [![NPM version](https://badge.fury.io/js/pg-ears.svg)](https://npmjs.org/package/pg-ears)   [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
+> Resilient Postgres listen client
+
+## install
+
+```sh
+$ npm install --save pg-ears
+```
+
+## api
+
+pg-ears exports a single function takes the same options as a new `node-postgres` `Client` and returns a promise that resolves with an object containing the methods `listen` and `notify`
+
+- **options** *(Object - required)* Options for [node-postgres](https://github.com/brianc/node-postgres) connection
+
+`listen(channel, callback)`
+- **channel** *(String - required)*
+- **callback** *(Function - required)* will be called every time a message is received with `(error, data)` as arguments
+
+`notify(channel, payload, callback)`
+- **channel** *(Object - required)*
+- **payload** *(Object | Array | String - required)*
+- **callback** *(Object - optional)* will be called with error if unable to send
+
+## usage
+
+```js
+const options = {
+  user: 'foo', //env var: PGUSER
+  database: 'my_db', //env var: PGDATABASE
+  password: 'secret', //env var: PGPASSWORD
+  host: 'localhost', // Server hosting the postgres database
+  port: 5432 //env var: PGPORT
+}
+const pgEars = require('pg-ears')(options)
+pgEars.listen('mychannel', (err, data) => {
+  if (err) return console.error(err)
+  console.log(data)
+})
+pgEars.notify('mychannel', {key: 'value'}, (err) => {
+  if (err) console.error(err)
+})
+```
+
+## license
+
+MIT © [Andrew Carpenter](https://github.com/doesdev)
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+// setup
+const Pg = require('pg').Client
+const maxAttempts = 60
+const attempts = {}
+
+// export
+module.exports = (opts) => new Promise((resolve, reject) => {
+  let listen = (channel, cb) => {
+    let client = new Pg(opts)
+    client.connect((err) => {
+      if (err) return cb(err)
+      client.connection.stream.setKeepAlive(true)
+      client.query(`LISTEN ${channel};`, (e) => {
+        if (e) return cb(e)
+      })
+      client.on('error', cb)
+      client.on('notification', (d) => {
+        if (d.toString() !== 'pg-ears-test') return cb(null, d)
+        attempts[channel] = 0
+      })
+      setInterval(
+        (() => {
+          let testClient = new Pg(opts)
+          let handleErr = (e) => {
+            attempts[channel] = attempts[channel] || 0
+            attempts[channel] += 1
+            if (client && client.end) client.end()
+            if (testClient && testClient.end) testClient.end()
+            if (attempts[channel] > maxAttempts) return cb(err)
+            return listen(channel, cb)
+          }
+          testClient.connect((err) => {
+            if (err) return handleErr(err)
+            testClient.query(`NOTIFY ${channel}, 'pg-ears-test';`, (err) => {
+              if (err) return handleErr(err)
+              if (testClient && testClient.end) testClient.end()
+            })
+          })
+        })(),
+        30000
+      )
+    })
+  }
+  let notify = (channel, payload, cb) => {
+    let client = new Pg(opts)
+    payload = JSON.stringify(payload)
+    client.connect((err) => {
+      if (err) return cb(err)
+      client.query(`NOTIFY ${channel}, '${payload}';`, (e) => {
+        if (e) return cb(e)
+        if (client && client.end) client.end()
+      })
+    })
+  }
+  resolve({listen, notify})
+})

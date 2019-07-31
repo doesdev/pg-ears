2dfa293a94e9cdbdb052fa3a3c7d517baf3cb612
Andrew Carpenter <andrew@audioinhd.com>
2019-07-30 22:44:42 -0400

    Use mvt for tests

--- test.js
+++ test.js
@@ -1,18 +1,24 @@
 'use strict'
 
-import test from 'ava'
-import pgEars from './index'
+const test = require('mvt')
+const pgEars = require('./index')
 const pgOptions = require('./../_secrets/pg-ears/config.json')
 const { listen, notify } = pgEars(pgOptions)
 const channel = 'my_test_channel'
 const msg = 'heydair'
 
-test.cb('it listens and notifies', (assert) => {
-  let gotIt = (err, pl) => {
-    if (err) throw err
-    assert.is(pl, msg)
-    assert.end()
-  }
-  listen(channel, gotIt)
-  setTimeout(() => notify(channel, msg, console.error), 2000)
+test('it listens and notifies', async (assert) => {
+  let val
+
+  await new Promise((resolve, reject) => {
+    let gotIt = (err, pl) => {
+      if (err) reject(err)
+      val = pl
+      resolve()
+    }
+    listen(channel, gotIt)
+    setTimeout(() => notify(channel, msg, console.error), 2000)
+  })
+
+  assert.is(msg, val)
 })
